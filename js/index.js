008d6ab1256428230f579b0fcfb2b878d022f440
unknown <aut_origin>
2021-04-02 16:58:56 +0800

    完整版轮播图

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 /*
  * @Author: your name
  * @Date: 2021-04-02 14:51:29
- * @LastEditTime: 2021-04-02 14:58:44
- * @LastEditors: your name
+ * @LastEditTime: 2021-04-02 16:29:00
+ * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \AAAf:\Web前端开发\项目\jiguo_clone\qy133-jigup\js\index.js
  */
@@ -47,18 +47,18 @@ let focus_index = 0;
 // 轮播函数
 function play(fu) {
   flag = false;
-  let left_num = -fu * (show_item[0].offsetWidth + 44) * focus_index * 4;
 
   //   如果轮播到最后四个，右侧按钮消失，反之，显现
-  if (focus_index == show_item.length / 4 - 1) {
-    next.style.display = "none";
+  if (focus_index == Math.ceil(show_item.length / 4 - 1)) {
+    document.querySelector(".show-next img").style.display = "none";
   } else if (focus_index == 0) {
-    prev.style.display = "none";
+    document.querySelector(".show-prev img").style.display = "none";
   } else {
-    next.style.display = "";
-    prev.style.display = "";
+    document.querySelector(".show-next img").style.display = "inline";
+    document.querySelector(".show-prev img").style.display = "inline";
   }
 
+  let left_num = fu * (show_item[0].offsetWidth + 44) * 4;
   animate(show, left_num, function () {
     flag = true;
   });
@@ -67,13 +67,16 @@ function play(fu) {
 next.onclick = function () {
   if (flag) {
     focus_index++;
-    play(1);
+    play(-focus_index);
+
+    console.log(focus_index);
   }
 };
 
 prev.onclick = function () {
   if (flag) {
     focus_index--;
-    play(-1);
+    play(-focus_index);
+    console.log(focus_index);
   }
 };
